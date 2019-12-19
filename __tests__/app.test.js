3fce96445b206512275ab4eb8faab0401f64b044
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-19 14:27:58 -0800

    added tests for subtypes and sets routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,8 @@ const Card = require('../lib/models/Card');
 
 describe('cards routes', () => {
   let card;
+  let card1;
+  let card2;
 
   beforeAll(() => {
     connect();
@@ -21,6 +23,26 @@ describe('cards routes', () => {
   beforeEach(async() => {
     card = await Card.create({
       name: 'Greatest Card',
+      set: 'lol',
+      setType: 'expansion',
+      cmc: 3,
+      releaseDate: '2020-01-24T08:00:00.000Z',
+      types: ['Creature'],
+      subtypes: ['Human', 'Warrior'],
+      colors: ['G']
+    });
+    card1 = await Card.create({
+      name: 'Card 1',
+      set: 'lol',
+      setType: 'expansion',
+      cmc: 3,
+      releaseDate: '2020-01-24T08:00:00.000Z',
+      types: ['Creature'],
+      subtypes: ['Human', 'Warrior'],
+      colors: ['G']
+    });
+    card2 = await Card.create({
+      name: 'Card 2',
       set: 'lol',
       setType: 'expansion',
       cmc: 3,
@@ -64,6 +86,39 @@ describe('cards routes', () => {
       });
   });
 
+  it('gets subtypes', () => {
+    return request(app)
+      .get('/api/v1/cards/subtypes/')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: 'Warrior',
+            avgCmc: 3,
+            total: 3,
+          },
+          {
+            _id: 'Human',
+            avgCmc: 3,
+            total: 3,
+          },
+        ]);
+      });
+  });
+
+  it('gets sets', () => {
+    return request(app)
+      .get('/api/v1/cards/sets/')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: 'lol',
+          avgCmc: 3,
+          maxCmc: 3,
+          total: 3,
+          releaseDate: '2020-01-24T08:00:00.000Z'
+        }]);
+      });
+  });
+
   it('gets a card by id', () => {
     return request(app)
       .get(`/api/v1/cards/${card.id}`)
@@ -99,7 +154,34 @@ describe('cards routes', () => {
             subtypes: ['Human', 'Warrior'],
             colors: ['G'],
             __v: 0
-          }
+          },
+          {
+            _id: card1.id,
+
+            name: 'Card 1',
+            set: 'lol',
+            setType: 'expansion',
+            cmc: 3,
+            releaseDate: '2020-01-24T08:00:00.000Z',
+            types: ['Creature'],
+            subtypes: ['Human', 'Warrior'],
+            colors: ['G'],
+            __v: 0
+
+          },
+          {
+            _id: card2.id,
+            name: 'Card 2',
+            set: 'lol',
+            setType: 'expansion',
+            cmc: 3,
+            releaseDate: '2020-01-24T08:00:00.000Z',
+            types: ['Creature'],
+            subtypes: ['Human', 'Warrior'],
+            colors: ['G'],
+            __v: 0
+
+          }     
         ]);
       });
   });
